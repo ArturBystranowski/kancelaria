3b9473c0c80a92dc381b16939d99fae8aced6775
ArturBystranowski <artur.bystranowski@gmail.com>
2022-08-28 02:54:34 +0200

    work on RWD

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-// const btnCriminal=document.querySelector('.service-criminal')
-// const bodyCriminal=document.querySelector('#service .service-body')
-
-// const openMenu=(e)=>{
-// console.log(e.target)
-// btnCriminal.classList.toggle('active')
-// bodyCriminal.classList.toggle('open')
-// }
-
-// btnCriminal.addEventListener('click',openMenu)
-
 const serviceList = document.querySelector(".service");
 const serviceSection = document.querySelectorAll(".service section");
+const hamBtn = document.querySelector(".nav-list");
+const hamMenu = document.querySelector(".nav-list-ham");
+
 const openMenu = (e) => {
 	if (e.target.matches(".service-header")) {
 		e.target.classList.toggle("active");
@@ -21,7 +13,16 @@ const openMenu = (e) => {
 				element.classList.remove("active");
 			}
 		});
+	} else {
+		serviceSection.forEach((element) => {
+			element.classList.remove("open");
+			element.classList.remove("active");
+		});
 	}
 };
 
 serviceList.addEventListener("click", openMenu);
+hamBtn.addEventListener("click", () => {
+	console.log("test");
+	hamMenu.classList.toggle("active");
+});
